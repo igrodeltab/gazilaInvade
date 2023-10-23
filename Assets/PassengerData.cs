dca7b14c33dcff8a4d6034373fd6f02b84338694
igrodeltab <igrodeltab@gmail.com>
2023-10-24 00:26:44 +0400

    add ticket price

---
+++ Assets/PassengerData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PassengerData", menuName = "Game/Passenger Data", order = 0)]
+public class PassengerData : ScriptableObject
+{
+    public int _paymentAmount; // Количество денег, которое пассажир платит за поездку
+
+    // Можно добавить другие атрибуты, характерные для пассажира
+}
---
+++ Assets/TicketPrice.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TicketPrice : MonoBehaviour
+{
+    [SerializeField]
+    private PassengerData _passengerData; // Ссылка на данные пассажира
+
+    // Метод для получения стоимости билета
+    public int GetFareAmount()
+    {
+        return _passengerData._paymentAmount;
+    }
+}
