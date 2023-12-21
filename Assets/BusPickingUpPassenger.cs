a1ed69649cac407b6357eed55aa1ccebbb185b35
igrodeltab <igrodeltab@gmail.com>
2023-12-21 11:12:32 +0400

    added passenger counter

--- Assets/BusPickingUpPassenger.cs
+++ Assets/BusPickingUpPassenger.cs
@@ -3,46 +3,44 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class BusPickingUpPassenger : MonoBehaviour
 {
-    [SerializeField] private int _ticketPrice; // Фиксированная цена за проезд, настраиваемая в инспекторе
-    [ReadOnly] [SerializeField] private int _totalEarnings; // Общая сумма денег, заработанная от пассажиров
-    
+    [SerializeField] private int _ticketPrice;
+    [ReadOnly] [SerializeField] private int _totalEarnings;
+    [SerializeField] private int _maxPassengers = 10; // Максимальное количество пассажиров
+    [ReadOnly] [SerializeField] private int _currentPassengers = 0; // Текущее количество пассажиров
+
     private Rigidbody2D _rigidbody2D;
-    private ShowTicketPrice _showTicketPrice; // Ссылка на скрипт ShowTicketPrice
+    private ShowTicketPrice _showTicketPrice;
 
     private void Awake()
     {
         _totalEarnings = 0;
+        _currentPassengers = 0;
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _showTicketPrice = GetComponent<ShowTicketPrice>();
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        // Проверяем, вошел ли в триггер объект с тегом "Passenger"
-        if (collider.CompareTag("Passenger"))
+        if (collider.CompareTag("Passenger") && _rigidbody2D.velocity.magnitude < 0.01f)
         {
-            // Проверяем, стоит ли автобус на месте
-            if (_rigidbody2D.velocity.magnitude < 0.01f) // автобус практически не движется
-            {
-                // "Подбираем" пассажира
-                PickUpPassenger(collider.gameObject);
-            }
+            PickUpPassenger(collider.gameObject);
         }
     }
 
     private void PickUpPassenger(GameObject passenger)
     {
-        // Добавляем стоимость проезда к общей сумме заработка
-        _totalEarnings += _ticketPrice;
-
-        //prefabSpawner.SpawnPrefabOnCanvas(passenger.transform.position);
-
-        if (_showTicketPrice != null)
+        if (_currentPassengers < _maxPassengers)
         {
-            _showTicketPrice.SpawnPrefabOnCanvas(passenger.transform.position, _ticketPrice);
-        }
+            _totalEarnings += _ticketPrice;
+            _currentPassengers++;
+            PassengerCounter.Instance?.UpdatePassengerCount(_currentPassengers, _maxPassengers);
 
-        // Логика по обработке пассажира, например, удаление объекта или другие действия
-        Destroy(passenger);
+            if (_showTicketPrice != null)
+            {
+                _showTicketPrice.SpawnPrefabOnCanvas(passenger.transform.position, _ticketPrice);
+            }
+
+            Destroy(passenger);
+        }
     }
 }
---
+++ Assets/PassengerCounter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PassengerCounter : MonoBehaviour
+{
+    private TextMeshProUGUI _passengerCounterText;
+
+    public static PassengerCounter Instance { get; private set; }
+
+    private void Awake()
+    {
+        _passengerCounterText = GetComponent<TextMeshProUGUI>();
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdatePassengerCount(int current, int max)
+    {
+        if (_passengerCounterText != null)
+        {
+            _passengerCounterText.text = $"{current}/{max}";
+        }
+    }
+}
