dd0f0f2265d855d4181908d2dfb799e3f24a3ebf
igrodeltab <igrodeltab@gmail.com>
2023-12-24 22:36:54 +0400

    added blinking for route points

---
+++ Assets/BlinkingPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BlinkingPoint : MonoBehaviour
+{
+    [SerializeField] private float _blinkInterval = 1f; // Интервал мигания в секундах
+    private Renderer _objectRenderer;
+    private float _timer;
+
+    private void Awake()
+    {
+        _objectRenderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _blinkInterval)
+        {
+            // Переключаем видимость объекта
+            _objectRenderer.enabled = !_objectRenderer.enabled;
+            _timer = 0f;
+        }
+    }
+}
