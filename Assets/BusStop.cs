412ddbcc1fab194b6993f3e5a7077d9083fdf45e
igrodeltab <igrodeltab@gmail.com>
2023-12-24 23:59:13 +0400

    changed drop off conditions from OnTriggerEnter2D to OnTriggerStay2D

--- Assets/BusStop.cs
+++ Assets/BusStop.cs
@@ -2,14 +2,38 @@
 
 public class BusStop : MonoBehaviour
 {
-    [SerializeField] private GameObject passengerPrefab; // Префаб пассажира
+    [SerializeField] private GameObject _passengerPrefab; // Префаб пассажира
+    private const float _minVelocityToUnload = 0.01f; // Минимальная скорость для высадки пассажиров
+    private BusPickingUpPassenger _bus;
+    private Rigidbody2D _busRigidbody;
+    private bool _passengersUnloaded = false; // Флаг, отслеживающий высадку пассажиров
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        BusPickingUpPassenger bus = collider.GetComponent<BusPickingUpPassenger>();
-        if (bus != null && bus.DropOffPassengerPoint != null)
+        _bus = collider.GetComponent<BusPickingUpPassenger>();
+        _busRigidbody = collider.GetComponent<Rigidbody2D>();
+        _passengersUnloaded = false; // Сбросить флаг при входе автобуса в остановку
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (!_passengersUnloaded && _bus != null && _bus.DropOffPassengerPoint != null && _busRigidbody != null)
         {
-            UnloadPassengers(bus, bus.DropOffPassengerPoint);
+            if (_busRigidbody.velocity.magnitude < _minVelocityToUnload)
+            {
+                UnloadPassengers(_bus, _bus.DropOffPassengerPoint);
+                _passengersUnloaded = true; // Установить флаг, чтобы избежать повторной высадки
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (_bus != null && collider.GetComponent<BusPickingUpPassenger>() == _bus)
+        {
+            _bus = null;
+            _busRigidbody = null;
+            _passengersUnloaded = false; // Сбросить флаг при выходе автобуса из остановки
         }
     }
 
@@ -18,7 +42,7 @@ public class BusStop : MonoBehaviour
         int passengersToUnload = Random.Range(0, bus.CurrentPassengers + 1);
         for (int i = 0; i < passengersToUnload; i++)
         {
-            Instantiate(passengerPrefab, dropOffPoint.position, Quaternion.identity);
+            Instantiate(_passengerPrefab, dropOffPoint.position, Quaternion.identity);
             bus.RemovePassenger();
         }
     }
