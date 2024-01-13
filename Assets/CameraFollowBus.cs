37479ec1f66d6bd76b9e25410fd9be8d256042ad
igrodeltab <igrodeltab@gmail.com>
2024-01-13 20:22:43 +0400

    added camera follow bus

---
+++ Assets/CameraFollowBus.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollowBus : MonoBehaviour
+{
+    [SerializeField] private Transform _busTransform; // Ссылка на трансформ автобуса
+
+    private void Update()
+    {
+        // Обновляем позицию камеры, следуя за автобусом, но сохраняя исходную Z-координату камеры
+        if (_busTransform != null)
+        {
+            transform.position = new Vector3(_busTransform.position.x, _busTransform.position.y, transform.position.z);
+        }
+    }
+}
