b374b5b43f8d222fdd963d86205766eb227e9eb3
igrodeltab <igrodeltab@gmail.com>
2024-01-13 20:43:33 +0400

    added direction for CameraFollowBus

--- Assets/CameraFollowBus.cs
+++ Assets/CameraFollowBus.cs
@@ -3,13 +3,36 @@
 public class CameraFollowBus : MonoBehaviour
 {
     [SerializeField] private Transform _busTransform; // Ссылка на трансформ автобуса
+    [SerializeField] private Transform _frontPointTransform; // Точка перед автобусом
+    [SerializeField] private Transform _backPointTransform; // Точка сзади автобуса
+    [SerializeField] private float _transitionSpeed = 1f; // Скорость перехода между целями
+
+    private Rigidbody2D _busRigidbody;
+
+    private void Start()
+    {
+        _busRigidbody = _busTransform.GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        // Обновляем позицию камеры, следуя за автобусом, но сохраняя исходную Z-координату камеры
-        if (_busTransform != null)
+        Transform targetTransform = DetermineTargetTransform();
+        Vector3 targetPosition = new Vector3(targetTransform.position.x, targetTransform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _transitionSpeed * Time.deltaTime);
+    }
+
+    private Transform DetermineTargetTransform()
+    {
+        if (_busRigidbody.velocity.magnitude < 0.1f) // Если автобус практически остановился
         {
-            transform.position = new Vector3(_busTransform.position.x, _busTransform.position.y, transform.position.z);
+            return _busTransform;
         }
+
+        // Определение направления движения автобуса (вперед или назад)
+        Vector2 busDirection = _busRigidbody.velocity.normalized;
+        Vector2 busForward = _busTransform.up; // Предполагаем, что 'up' трансформа автобуса направлено вперед
+        bool movingForward = Vector2.Dot(busDirection, busForward) > 0;
+
+        return movingForward ? _frontPointTransform : _backPointTransform;
     }
 }
