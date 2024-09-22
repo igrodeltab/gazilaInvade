b5d6c2976b7bb8c9c2a06716dc78d79e8d8cc33f
igrodeltab <igrodeltab@gmail.com>
2024-09-22 15:05:25 +0400

    added TagSelectorAttribute and TagSelectorPropertyDrawer

---
+++ Assets/Scripts/TagSelectorAttribute.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class TagSelectorAttribute : PropertyAttribute
+{
+    // Атрибут для указания, что это поле должно отображаться как список тегов
+}
---
+++ Assets/Scripts/TagSelectorPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+public class TagSelectorPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType == SerializedPropertyType.String)
+        {
+            // Проверяем, что это строка и отображаем как тег
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Показываем выпадающий список с тегами
+            property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+
+            EditorGUI.EndProperty();
+        }
+        else
+        {
+            // Показываем сообщение об ошибке, если тип не строковый
+            EditorGUI.PropertyField(position, property, label);
+            Debug.LogWarning("TagSelectorAttribute can only be used on string properties.");
+        }
+    }
+}
