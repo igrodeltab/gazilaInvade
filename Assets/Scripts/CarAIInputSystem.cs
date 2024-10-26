31a79d66c1671beee212944c9dfa60a2cbb2dae4
igrodeltab <igrodeltab@gmail.com>
2024-10-26 22:50:48 +0400

    adjusted rule for choosing left and right turn for car ai controller

--- Assets/Scripts/CarAIInputSystem.cs
+++ Assets/Scripts/CarAIInputSystem.cs
@@ -2,9 +2,9 @@
 
 public class CarAIInputSystem : MonoBehaviour
 {
-    [SerializeField] private TileTriggerChecker _frontTileTriggerChecker; // Reference to TileTriggerChecker
-    [SerializeField] private TileTriggerChecker _rightTileTriggerChecker;
-    [SerializeField] private TileTriggerChecker _leftTileTriggerChecker;
+    [SerializeField] private TileTriggerChecker _frontTileTriggerChecker; // Reference to front TileTriggerChecker
+    [SerializeField] private TileTriggerChecker _rightTileTriggerChecker; // Reference to right TileTriggerChecker
+    [SerializeField] private TileTriggerChecker _leftTileTriggerChecker;  // Reference to left TileTriggerChecker
     [SerializeField] private CarMovement _carMovement; // Reference to CarMovement component
 
     private void Update()
@@ -14,19 +14,24 @@ public class CarAIInputSystem : MonoBehaviour
         {
             _carMovement.MoveForward(); // Command to move forward
         }
-        else if (_rightTileTriggerChecker.TileCountInArea >= 2)
-        {
-            _carMovement.MoveForward();
-            _carMovement.TurnRight();
-        }
-        else if (_leftTileTriggerChecker.TileCountInArea >= 2)
-        {
-            _carMovement.MoveForward();
-            _carMovement.TurnLeft();
-        }
         else
         {
-            _carMovement.Stop(); // Command to stop
+            // Compare right and left tile counts to determine the better turn direction
+            if (_rightTileTriggerChecker.TileCountInArea > _leftTileTriggerChecker.TileCountInArea)
+            {
+                _carMovement.MoveForward();
+                _carMovement.TurnRight();
+            }
+            else if (_leftTileTriggerChecker.TileCountInArea > _rightTileTriggerChecker.TileCountInArea)
+            {
+                _carMovement.MoveForward();
+                _carMovement.TurnLeft();
+            }
+            else
+            {
+                // If both sides are equal or no tiles, choose a default turn or stop
+                _carMovement.Stop(); // or _carMovement.TurnRight() if you prefer a default turn
+            }
         }
     }
 }
