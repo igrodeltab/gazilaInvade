e29c668d6fbfd5d5e7c2712a54862628e153a9c8
igrodeltab <igrodeltab@gmail.com>
2024-11-03 16:59:14 +0400

    added forward detection for car

--- Assets/Scripts/CarAIInputSystem.cs
+++ Assets/Scripts/CarAIInputSystem.cs
@@ -6,49 +6,58 @@ public class CarAIInputSystem : MonoBehaviour
     [SerializeField] private TileTriggerChecker _rightTileTriggerChecker; // Reference to right TileTriggerChecker
     [SerializeField] private TileTriggerChecker _leftTileTriggerChecker;  // Reference to left TileTriggerChecker
     [SerializeField] private CarMovement _carMovement; // Reference to CarMovement component
+    [SerializeField] private ForwardDetection _forwardDetection; // Reference to ForwardDetection component
 
     private enum TurnDirection { None, Right, Left }
     private TurnDirection _currentTurnDirection = TurnDirection.None;
 
     private void Update()
     {
-        // Check if there are fewer than 8 tiles in front
-        if (_frontTileTriggerChecker.TileCountInArea < 6)
+        // If there is something ahead, stop the car
+        if (_forwardDetection.IsSomethingAhead)
         {
-            // If no turn direction is set, determine the better turn direction
-            if (_currentTurnDirection == TurnDirection.None)
-            {
-                if (_rightTileTriggerChecker.TileCountInArea > _leftTileTriggerChecker.TileCountInArea)
-                {
-                    _currentTurnDirection = TurnDirection.Right;
-                }
-                else if (_leftTileTriggerChecker.TileCountInArea > _rightTileTriggerChecker.TileCountInArea)
-                {
-                    _currentTurnDirection = TurnDirection.Left;
-                }
-                else
-                {
-                    _currentTurnDirection = TurnDirection.Right; // Default turn
-                }
-            }
-
-            // Execute the current turn direction
-            if (_currentTurnDirection == TurnDirection.Right)
-            {
-                _carMovement.TurnRight();
-            }
-            else if (_currentTurnDirection == TurnDirection.Left)
-            {
-                _carMovement.TurnLeft();
-            }
+            _carMovement.Stop();
         }
         else
         {
-            // If there are 8 or more tiles in front, reset the turn direction
-            _currentTurnDirection = TurnDirection.None;
-        }
+            // Check if there are fewer than 8 tiles in front
+            if (_frontTileTriggerChecker.TileCountInArea < 6)
+            {
+                // If no turn direction is set, determine the better turn direction
+                if (_currentTurnDirection == TurnDirection.None)
+                {
+                    if (_rightTileTriggerChecker.TileCountInArea > _leftTileTriggerChecker.TileCountInArea)
+                    {
+                        _currentTurnDirection = TurnDirection.Right;
+                    }
+                    else if (_leftTileTriggerChecker.TileCountInArea > _rightTileTriggerChecker.TileCountInArea)
+                    {
+                        _currentTurnDirection = TurnDirection.Left;
+                    }
+                    else
+                    {
+                        _currentTurnDirection = TurnDirection.Right; // Default turn
+                    }
+                }
 
-        // Always move forward
-        _carMovement.MoveForward();
+                // Execute the current turn direction
+                if (_currentTurnDirection == TurnDirection.Right)
+                {
+                    _carMovement.TurnRight();
+                }
+                else if (_currentTurnDirection == TurnDirection.Left)
+                {
+                    _carMovement.TurnLeft();
+                }
+            }
+            else
+            {
+                // If there are 8 or more tiles in front, reset the turn direction
+                _currentTurnDirection = TurnDirection.None;
+            }
+
+            // Move forward only if there is no obstacle
+            _carMovement.MoveForward();
+        }
     }
 }
---
+++ Assets/Scripts/ForwardDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ForwardDetection : MonoBehaviour
+{
+    //public bool IsSomethingAhead { get; private set; } = false;
+    public bool IsSomethingAhead = false;
+    [SerializeField] private float detectionWidth = 5f;  // Width of the rectangular area
+    [SerializeField] private float detectionHeight = 5f; // Height of the rectangular area
+    [SerializeField] private LayerMask detectionLayer;   // Layer to filter detected objects
+
+    private void Update()
+    {
+        // Define the size of the rectangular detection area
+        Vector2 boxSize = new Vector2(detectionWidth, detectionHeight);
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, boxSize, 0f, detectionLayer);
+        IsSomethingAhead = colliders.Length > 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Set the Gizmos color to magenta
+        Gizmos.color = Color.magenta;
+        // Draw the detection area as a wireframe rectangle
+        Gizmos.DrawWireCube(transform.position, new Vector3(detectionWidth, detectionHeight, 0f));
+    }
+}
