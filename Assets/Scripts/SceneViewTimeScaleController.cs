400167019bc01c9fbef4109fcf46625c39382c3f
igrodeltab <igrodeltab@gmail.com>
2024-11-10 18:39:16 +0400

    added Pause Control and Step to Next Frame to SceneViewTimeScaleController

--- Assets/Scripts/SceneViewTimeScaleController.cs
+++ Assets/Scripts/SceneViewTimeScaleController.cs
@@ -13,25 +13,45 @@ public class SceneViewTimeScaleController
     {
         Handles.BeginGUI();
 
-        GUILayout.BeginArea(new Rect(10, 10, 150, 130), "Time Scale", GUI.skin.window);
+        GUILayout.BeginArea(new Rect(10, 10, 150, 200), "Time Control", GUI.skin.window);
 
         GUILayout.Label("Control Game Time Scale", EditorStyles.boldLabel);
 
         if (GUILayout.Button("Normal Speed"))
         {
             Time.timeScale = 1f;
+            EditorApplication.isPaused = false;
         }
         if (GUILayout.Button("Slow Down x2"))
         {
             Time.timeScale = 0.5f;
+            EditorApplication.isPaused = false;
         }
         if (GUILayout.Button("Slow Down x4"))
         {
             Time.timeScale = 0.25f;
+            EditorApplication.isPaused = false;
         }
         if (GUILayout.Button("Slow Down x8"))
         {
             Time.timeScale = 0.125f;
+            EditorApplication.isPaused = false;
+        }
+
+        GUILayout.Space(10);
+
+        GUILayout.Label("Pause Control", EditorStyles.boldLabel);
+
+        if (GUILayout.Button(EditorApplication.isPaused ? "Unpause" : "Pause"))
+        {
+            EditorApplication.isPaused = !EditorApplication.isPaused;
+        }
+        if (GUILayout.Button("Step to Next Frame"))
+        {
+            if (EditorApplication.isPaused)
+            {
+                EditorApplication.Step();
+            }
         }
 
         GUILayout.EndArea();
