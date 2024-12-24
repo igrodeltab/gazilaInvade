86ee6fc412c69126e83cfc2c063318581a8e08dd
igrodeltab <igrodeltab@gmail.com>
2024-12-24 18:28:25 +0400

    fixed local rotation for detection zone of car (square)

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -5,18 +5,17 @@ public class DetectionZone : MonoBehaviour
     [SerializeField] private Vector2 _zoneSize = new Vector2(5f, 3f);
 
     public bool IsObjectDetected { get; private set; }
-    [SerializeField] private bool _isDetected;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, _zoneSize);
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, _zoneSize);
     }
 
     private void Update()
     {
-        Collider2D[] detectedObjects = Physics2D.OverlapBoxAll(transform.position, _zoneSize, 0f);
+        Collider2D[] detectedObjects = Physics2D.OverlapBoxAll(transform.position, _zoneSize, transform.eulerAngles.z);
         IsObjectDetected = detectedObjects.Length > 0;
-        _isDetected = IsObjectDetected;
     }
 }
